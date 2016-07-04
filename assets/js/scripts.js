56cc7343f7958674d439c597a7321799b1d5db27
Denis Ricci <denis.riccis@gmail.com>
2016-07-03 21:39:32 -0300

    first version using ajax with spring security

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,64 @@
-window.onload=function(){
-	sendRequest("http://localhost:8080/api/post/page/1");
+window.onload = function() {	
+	login();	
 };
 
-function sendRequest(url, method = "GET", body){
+function getPosts() {
+	sendRequest('http://localhost:8080/api/post/page/1', {
+		withCredentials : true
+	});
+}
+
+function login() {
+	sendRequest('http://localhost:8080/login', {
+		method : 'POST',
+		async : false,
+		contentType : 'application/x-www-form-urlencoded',
+		body : 'username='.concat(config.username).concat('&password=').concat(
+				config.password)
+	});
+}
+
+function sendRequest(url, options) {
+	settings = {
+		method : 'GET',
+		withCredentials : true,
+		async : true,
+		contentType : 'application/json',
+		body : ''
+	}
+	
+	var promise = new Promisse();
+
+	settings = extend(options, settings);
+
 	var request = new XMLHttpRequest();
-	request.open(method, url);
+	if (settings.async) {
+		request.open(settings.method, url, true);
+	} else {
+		request.open(settings.method, url);
+	}
+	request.withCredentials = settings.withCredentials;
+	request.setRequestHeader('Content-Type', settings.contentType);
 	request.onreadystatechange = function() {
 		if (request.readyState == XMLHttpRequest.DONE) {
-			alert(request.status);
+			alert(request.status + ' ' + url);
+			if(url !== 'http://localhost:8080/api/post/page/1'){
+				getPosts();
+			}
 		}
 	}
-	request.setRequestHeader("Authorization", "Basic dHJlbGQ6dHJlbGQ=");
-	request.send();
+
+	request.send(settings.body);
+
+}
+
+function extend(obj1, obj2) {
+	if (obj1 == undefined) {
+		return obj2;
+	}
+
+	for ( var attrName in obj1) {
+		obj2[attrName] = obj1[attrName];
+	}
+	return obj2;
 }
