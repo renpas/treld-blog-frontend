282c084019b1dd4179c9f4ac31ff116be51b4425
Thais Arcanjo <arcanjo@gmail.com>
2016-07-25 23:47:46 -0300

    dashboard replaceall

--- assets/js/managePosts.js
+++ assets/js/managePosts.js
@@ -1,19 +1,25 @@
+var tableLists = null;
+
 window.onload = function () {
-    loadPosts();
+    loadPosts(1);
    
 };
 
-function loadPosts() {
+function loadPosts(page) {
     var tableList = document.querySelector("#tableList > tbody");
     ajax(paths.managePostTemplate).then(function (template) {
-        ajax(config.urlServer.concat('/api/post/page/1'), {
+        ajax(config.urlServer.concat('/api/post/page/'.concat(page)), {
             withCredentials: true
         }).then(function (arrPosts) {
             tableList.innerHTML += buildRow(arrPosts, template);
-             var table = $('#tableList').DataTable({
+             tableLists = $('#tableList').DataTable({
              	"info":     false,
              	"bLengthChange": false ,
-             	"pageLength": 10
+             	"pageLength": 10,
+                "retrieve": true /*,
+                "processing": true,
+                "serverSide": true,
+                "ajax": "../".concat('/api/post/page/').concat(tableLists.page.info().page + 1)*/
              });
         });
     });
@@ -23,13 +29,18 @@ function buildRow(arrPosts, template) {
     var strAllPosts = "";
     arrPosts.forEach(function (post) {
         var strPost = template;
-        strPost = strPost.replace('@id', '<p>'.concat(post.id).concat('</p>'));
-        strPost = strPost.replace('@title', post.title);
-        strPost = strPost.replace('@author', post.author);
-        strPost = strPost.replace('@createdDate', milisecondsToDate(post.creationDate));
-        strPost = strPost.replace('@publishedDate', milisecondsToDate(post.publicationDate));
-        strPost = strPost.replace('@url', post.url);
+        strPost = strPost.replaceAll('@id', post.id)
+                .replaceAll('@title', post.title)
+                .replaceAll('@author', post.author)
+                .replaceAll('@createdDate', milisecondsToDate(post.creationDate))
+                .replaceAll('@publishedDate', milisecondsToDate(post.publicationDate))
+                .replaceAll('@url', post.url);
         strAllPosts += strPost;
     });
     return strAllPosts;
 }
+
+$('#tableList').on( 'page.dt', function () {
+    //pages are zero index based
+    loadPosts(tableLists.page.info().page + 1);
+} );
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,6 +2,9 @@
 JavaScript Style guide: https://github.com/airbnb/javascript/tree/master/es5
 */
 
+//toda String terá um String replaceAll na instância
+String.prototype.replaceAll=replaceAll;
+
 function milisecondsToDate(miliseconds) {
     var today = new Date();
     var dd = today.getDate();
@@ -18,4 +21,8 @@ function formatDate(dd, mm, yyyy) {
         mm = '0' + mm
     }
     return dd + '/' + mm + '/' + yyyy;
+}
+
+function replaceAll(find, replace) {
+    return this.replace(new RegExp(find, 'g'), replace);
 }
