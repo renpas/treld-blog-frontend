2c3bd44ba9cbd1bd678d571585ae2c660cba167a
Renã Passos de Souza <ren.boxe@gmail.com>
2016-07-31 18:17:40 -0300

    Moving original files

--- README.md
+++
@@ -1,1 +0,0 @@
-# treld-blog-frontend
--- assets/js/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	username: 'treld',
-	password: 'treld',
-	staticPath: 'http://localhost:8081/treld-blog-frontend/'
-}
-
-var paths = {
-	postTemplate: config.staticPath.concat("templates/post_template.html")
-}
